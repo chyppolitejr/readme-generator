cf5fbf0d6dee420c4719d21cce7bc28f46e24297
chyppoli <claude.hyppolite@gmail.com>
2020-11-01 10:09:15 -0500

    add licenseType array. update writeToFile and init functions

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const questions = [
   "What license type does this project use?",
 ];
 
+const licenseType = [
+  "none",
+  "Apache 2.0",
+  "GNU General Public v3.0",
+  "MIT",
+  "BSD 2-Clause 'Simplified'",
+  "BAS 3-Clause 'Simplified'",
+  "Boost Software 1.0",
+  "Creative Commons Zero v1.0 Universal",
+  "Eclipse Public 2.0",
+  "GNU Affero General Public v3.0",
+  "GNU General Public v2.0",
+  "GNU Lesser General Public v2.1",
+  "Mozilla Public 2.0",
+  "The Unilicense",
+];
+
 // function to write README file
 function writeToFile(fileName, data) {
   let fileName = "README.md";
@@ -23,7 +40,41 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {}
+function init() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "projName",
+      message: questions[0],
+    },
+    {
+      type: "input",
+      name: "projDesc",
+      message: questions[1],
+    },
+    {
+      type: "input",
+      name: "projInstInstr",
+      message: questions[2],
+    },
+    {
+      type: "input",
+      name: "projUse",
+      message: questions[3],
+    },
+    {
+      type: "input",
+      name: "projContrib",
+      message: questions[4],
+    },
+    {
+      type: "list",
+      name: "projLicense",
+      message: questions[5],
+      choices: [],
+    },
+  ]);
+}
 
 // function call to initialize program
 init();
